4574acd3503c5c8c9792ff42ce97bd140b46fdb5
Austin Jones <acjones212@gmail.com>
2024-07-22 12:27:59 -0400

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Austin Jones on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
